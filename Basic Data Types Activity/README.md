2b745c9c6cdddf144fefa5aea3263cc50ab1dfc9
happypancakec <happypancake3322@gmail.com>
2022-07-06 15:07:20 -0700

    cancel

--- Basic Data Types Activity/README.md
+++ Basic Data Types Activity/README.md
@@ -1,1 +1,2 @@
 # Working with Basic Data Types Activity
+this is a story I made using JavaScript
---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,118 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let girl = 'Sabrina';
+let suspect1 = 'Lucréce'
+let suspect2 = 'Filius'
+let name1 = 'Eleanor'
+let name2 = 'Simon'
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 1946
+let time = 1918
+let death = 1924
+
+
+
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let storyItems = ['Map', 'Shoe', 'Lake', 'Notebook' ]
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let hasPicture = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log("hello my name is" + name1 + " " +"Westcott" + ".");
+console.log("me and my partner,"+ " " + name2 + " "+ "Ashworth" + "," + " " +"run a private detective agency together" + "." );
+console.log("over the years"+ " " + name2 + " " +"and I have investigated some of the most bizarre and obsure cases in the world" + ".");
+console.log("However there's one case that I still think about to this day" +"." +" " + "." + "That case is the disappearance of " + girl + " " + "flipside" + ".");
+console.log("it was june 15"+ " " + year + " I was in the office organizing some documents when, all of a sudden I heard someone knocking on the door" + "." );
+console.log(name1 + ":" + "hold on, I'll be right there!");
+console.log("however when I went to open the the door no one was there" +".");
+console.log(name1 +":" + "huh?, there's no one here? Oh well I better get back to work!");
+console.log("But just then, I noticed there was a myterious letter right by my feet" + ".");
+console.log(name1 + ":" + "wait a minute, what's this!");
+console.log("I picked up the letter. the envelope glisten in the sunlight light as if it was made out of gold. the letter was sealed with a red and gold wax stamp. on the back our names were written with black ink" + ".");
+console.log("I took the letter and brought it into the office" + "." + " " + " As I was trying to figure out the origin of the letter" + " " + name2 + " " + "walked over to me to see what I was up to" +".");
+console.log(name2 + ":" + "hey" + " " + name1 + " " + "where'd you get that letter?");
+console.log(name1 + ":" + "someone left it on the doorstep of the office I'm trying to figure out who sent it" + ".");
+console.log(name2 + ":" +"why don't you just open it?");
+console.log(name1 + ":" + "Hmm, I don't know, what if it's for someone else who happens to have the same names as us?");
+console.log(name2 + ":" + "but what if it's not?");
+console.log(name1 + ":" + "good point");
+console.log("I sat there for a little while and finally made up my mind, I decided to open the letter" + ".");
+console.log("I opened the envelope and inside was a letter" + "." + "Here's what the letter said" + ".");
+console.log("Dear, WestCott & Ashworth agency, something very devastating has happened last night. our lovely daughter," + girl + " " + "has gone missing" + ".");
+console.log("We have search day and night for here but she is no where to be found" + "." + "she was last seen at our summer home in Apollo's Cove" + "." + " " + "Since Apollo's Cove is far from where you live we will pay for all of your travel expenses if you agree to come");
+console.log("for the sake of our daugther's well-being we are begging you please come help us. we need all the help we can get" + ".");
+console.log("Sincerely, the Lord and Lady of Apollo's Manor");
+console.log(name2 + ":" + "Well what do you think? should we take the case?");
+console.log(name1 + ":" + "Hmm, I've never heard of Apollo's cove before but I'm alawys open to new experiences!" );
+console.log(name2 + ":" + "alright, let's pack our bags and head over there!");
+console.log("That day I replyed to the Lord and Lady of Apollo's Manor agreeing to give the distressed couple our help" + ".");
+console.log("Also after some reaseach I figured out the only way to get there was by boat so we packed our bags and got on the next boat ride to Apollo's Cove" + ".");
+console.log("it was the afternoon when we arrived at Apollo's Cove" + "." + " " + name2 + " " + "and I were the only passengers on board which was rather odd since from what I could see Apollo's Cove was a hot tourist destination" + ".");
+console.log ("as we were getting closer to the dock I could see a ton of people hanging out at the beach" + ".");
+console.log("There were childeren making sandcastles and splashing eachother in the water" + ".");
+console.log("looking at all those people having fun at the beach made me wonder why i've never heard of this place before?");
+console.log("we arrive at the dock" + "." + " " + "As we got off the boat a friendly person wearing a tour guide shirt gave us a"+ " " + storyItems[0] + "." );
+console.log("we caught a taxi and headed to where we needed to go");
+console.log("Taxi driver: how are you enjoying Apollo's Cove so far?");
+console.log(name1 + ":" + "Well it's definely very nice here maybe one day I will go here for a non business trip" + ".");
+console.log("taxi driver: Oh what kind of business do you do?");
+console.log(name1 + ":" + " me and my partner here run a private detective agency together" + "."+ "  " + "Infact, we're here because of a case " + ".");
+console.log("Taxi driver: guessing by where we're heading you got hired to investigate the disappearance of that girl"+ " " + girl +".");
+console.log(name1 + ":" + "Oh, do you know anything about the case?");
+console.log("Taxi driver: Of course I do, it's the one thing everyone won't stop talking about! although no one really knows for sure what exactly what happened that night" + ".");
+console.log(name1 + ":" + "Hmm that seems very odd" + ".");
+console.log("Taxi driver: although all the stuff I heard about the situation is just rumors there's one thing I think might be true" + ".");
+console.log(name1 + ":" + " and what's that?");
+console.log("Taxi driver: Well, I've heard a lot of rumors about her boyfriend" + " " + suspect1 + "." + " " + "I heard ever since he came back from the war his relationship with" + " " + girl + "has been going downhill. Also I heard something about the" + " " + storyItems[2] + "that's in front of the summer home.");
+console.log("during the car ride me and the taxi driver talked the whole time the rumors going around town about this case" + "." + " " + "Althought they were just rumors, I still wrote them down in my" + " " + storyItems[3] + " " + " because I figured these rumors could give me some potential leads" + ".");
+console.log("finally after a while we made it to the front gates of Apollo's Manor" + "." + " " + "Outside of the front gates there were a ton of journalist and News reporters" + "."); 
+console.log("Our Taxi driver droped us off at the front gates and we went inside" + "."+ " " + "Apollo's Manor is a very interesting place, from my research Apollo's manor was built in"+ " " + time + " " + "the house originally belonged to a founder of the town until his death in" + " " + death + "." );
+console.log(name2 + " " + "and I knocked on the door of the Manor" + "." + " " + "A maid opened the door and let us inside" + "." + " " + "when we walked inside we went toward the living room" + ".");
+console.log("Inside the living room we saw a man in a fancy black suit with his head low" + "." + " " + "The man in the black suit wrapped his right arm around a women in a black gown who was hunched over and sobbing" + "." + " " + "on the left side of the sobbing women was a little girl also dressed in black" + ".");
+console.log("I walked over to the man and asked him");
+console.log(name1 + ":" + "Umm, excuse me sir are you the lord of Apollo's Manor?");
+console.log("The man in the fancy black suit: Why yes, I am the Lord of this Manor! and who are you two?");
+console.log(name2 + ":" + "I'm" + " " + name2 + " " + "Ashworth and she's" + " " + name1 + " " + "Westcott" + "." + " " + "We are from the WestCott & Ashworth agency " + ".");
+console.log("The man in the fancy black suit: Oh yes that's right you are those private detectives I hired! Welcome to the Manor, my name is Nikolai II this is my wife Winifred and over there is my youngest daugther Lotia" + ".");
+console.log(name1 + ":" + "Well it's nice to meet you and your family but if you don't mine me asking can you recall what happened the night your eldest daugther went missing?");
+console.log("Nikolai II: Me and my wife were not there that night. the only other people who were there was" + " " + girl + "," + " " + suspect1 + " " + ", and" + " " + "Lotia" + ".");
+console.log("Winifred: actually come to think of before we left I remember I saw" +" " + suspect2 + " " + "there that night as well");
+console.log(name2 + ":" + "who is" + " " + suspect2 + "?");
+console.log("Winifred: Oh, he is one of garderners. him and his father do all of our gardening" + "." + " " + "Also" + " " + girl + " " + "and" + " " + suspect2 + " " + "were very good friends" + ".");
+console.log(name2 + ":" + "Why do you refer their friendship in the past tense?");
+console.log ("Winifred: because a few days pior they stoped talking to each other" + ".");
+console.log(name2 + ":" + "Why did they stop talking to each other?");
+console.log("Winifred: I don't know.");
+console.log (name1 + ":" + "Well I guess we should start investigating" + ".");
+console.log (" Me and my partner were about to walk out of the living room when little Lotia ran up to me with something in her hand.");
+console.log("Lotia: Wait, Miss Westcott you forgot something!");
+console.log("the little girl gave me a picture of her older sister" + ".");
+console.log(name1 + ":" + "Oh why thank you little girl" + "!");
+console.log("Lotia: your welcome Miss Westcott" + ".");
+console.log("After that the little girl ran back to living room"+ ".");
+console.log("I was so optimistic about solving this case espically since we had a" + " " + hasPicture + " " + "picture of the missing person however I didn't realize this case was about to get a lot weirder" + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
